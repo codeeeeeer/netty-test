df8ea2ba2329f73ba575314071ce6a60b74e40a8
codeeeeeer <1076472061@qq.com>
2020-01-17 15:25:45 +0800

    init project

---
+++ src/test/java/test/netty/TestLog.java
@@ -0,0 +1,18 @@
+package test.netty;
+
+import junit.framework.TestCase;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 〈〉
+ *
+ * @author liujie
+ * @create 2020/01/17 15:16
+ */
+@Slf4j
+public class TestLog extends TestCase {
+
+    public void testLog(){
+        log.info("test message !");
+    }
+}
